ff7fc5539dc141fc84640059742c090750822a61
Hayden Setlik <haydensetlik@gmail.com>
2024-05-13 11:06:01 -0600

    added OLED driver

--- Core/Inc/Wavetable.h
+++ Core/Inc/Wavetable.h
@@ -10,7 +10,21 @@
 
 #include "stm32f4xx_hal.h"
 
-// because  we only have
+// tentatively storing our waves as 16-bit samples,
+
+#define WAVE_POINTS 512
+
+// this means that each wavetable takes up 512 * 16 bits = 1024 bytes
+// We have 32kB of RAM total, but let's reserve ~4kB for the stack and any other
+// memory needs. Each channels gets half of that remaining 28kB for a maximum
+// of 13 wave shapes per channel
+
+class Wave
+{
+private:
+	int16_t  data[WAVE_POINTS];
+
+};
 
 
 
